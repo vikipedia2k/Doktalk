1fa79d6a82992c5e48925a07a11df1b04ce928ce
vivek <vvekalotkar2000@gmail.com>
2022-07-11 23:00:48 +0530

    chnages

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "myhmsdb");
+    $server = "sql209.epizy.com" ;
+    $username = "epiz_31736669" ;
+    $password = "R5cdMJuW6R9Pk" ;
+    $dbname = "epiz_31736669_doktalk" ;
+$con = mysqli_connect($server, $username, $password, $dbname);
 
-if(mysqli_connect_errno()){
-    echo "Failed to connect: " . mysqli_connect_errno();
+if(mysqli_connect_error()){
+    echo "Failed to connect: " . mysqli_connect_error());
 }
--- myhmsdb.sql
+++ myhmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2021 at 07:33 AM
+-- Generation Time: Jun 14, 2021 at 08:20 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -66,9 +66,9 @@ CREATE TABLE `appointmenttb` (
 --
 
 INSERT INTO `appointmenttb` (`pid`, `ID`, `fname`, `lname`, `gender`, `email`, `contact`, `doctor`, `docFees`, `appdate`, `apptime`, `userStatus`, `doctorStatus`) VALUES
-(12, 14, 'Akash', 'Sawarkar', 'Male', 'akashsawarkar@gmail.com', '9986896896', 'Sneha', 350, '2021-05-28', '14:00:00', 1, 1),
-(12, 15, 'Akash', 'Sawarkar', 'Male', 'akashsawarkar@gmail.com', '9986896896', 'Sneha', 350, '2021-05-29', '16:00:00', 1, 1),
-(12, 16, 'Akash', 'Sawarkar', 'Male', 'akashsawarkar@gmail.com', '9986896896', 'Sneha', 350, '2021-05-30', '16:00:00', 1, 1);
+(14, 17, 'Vivek', 'Salotkar', 'Male', 'viveksalotkar@gmail.com', '9865321456', 'Sneha', 350, '2021-06-18', '08:00:00', 1, 1),
+(14, 18, 'Vivek', 'Salotkar', 'Male', 'viveksalotkar@gmail.com', '9865321456', 'Sneha', 350, '2021-06-25', '10:00:00', 1, 1),
+(14, 19, 'Vivek', 'Salotkar', 'Male', 'viveksalotkar@gmail.com', '9865321456', 'Sneha', 350, '2021-06-23', '12:00:00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -98,7 +98,8 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 ('Asiq', 'asiq@gmail.com', '9087897564', 'Love your service. Thank you!'),
 ('Jane', 'jane@gmail.com', '7869869757', 'I love your service!'),
 ('Sunil Chauhan', 'sunils@gmail.com', '9966633355', 'Very good experience .. Thank you !'),
-('Manasi', 'manasi@gmail.com', '9998686939', 'How can I choose doctor?');
+('Manasi', 'manasi@gmail.com', '9998686939', 'How can I choose doctor?'),
+('Manasi', 'manasi@gmail.com', '9865326556', 'Nice Experience !');
 
 -- --------------------------------------------------------
 
@@ -107,6 +108,7 @@ INSERT INTO `contact` (`name`, `email`, `contact`, `message`) VALUES
 --
 
 CREATE TABLE `doctb` (
+  `image` varchar(255) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -122,8 +124,10 @@ CREATE TABLE `doctb` (
 -- Dumping data for table `doctb`
 --
 
-INSERT INTO `doctb` (`username`, `password`, `email`, `city`, `pincode`, `location`, `experience`, `spec`, `docFees`) VALUES
-('Sneha', 'Sneha123', 'sneha@gmail.com', 'Pune', '411058', 'Warje', 'MBBS, DNB', 'Neurologist', 350);
+INSERT INTO `doctb` (`image`, `username`, `password`, `email`, `city`, `pincode`, `location`, `experience`, `spec`, `docFees`) VALUES
+('', 'Sneha', 'Sneha123', 'sneha@gmail.com', 'Pune', '411058', 'Warje', 'MBBS, DNB', 'Neurologist', 350),
+('', 'Manoj', 'Manoj123', 'manoj@gmail.com', 'Pune', '411023', 'Warje', 'MBBS', 'Pediatrician', 450),
+('cardback.jpeg', 'Dr Yash Patil', 'Yash123', 'yash@gmail.com', 'Hydrabad', '442001', 'Amirpeth', 'MBBS, MD', 'Cardiologist', 450);
 
 -- --------------------------------------------------------
 
@@ -136,6 +140,40 @@ CREATE TABLE `images` (
   `image_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `image_url`) VALUES
+(29, 'ambulance.png'),
+(30, 'favicon.png'),
+(31, 'favicon.png'),
+(32, 'favicon.png'),
+(33, 'favicon.png'),
+(34, 'favicon.png'),
+(35, 'ambulance.png'),
+(36, 'ambulance.png'),
+(37, 'ambulance.png'),
+(38, 'ambulance.png'),
+(39, 'ambulance.png'),
+(40, 'favicon.png'),
+(41, 'favicon.png'),
+(42, 'favicon.png'),
+(43, 'favicon.png'),
+(44, 'favicon.png'),
+(45, ''),
+(46, ''),
+(47, 'favicon.png'),
+(48, 'favicon.png'),
+(49, 'user2.png'),
+(50, 'user2.png'),
+(51, 'favicon.png'),
+(52, 'user4.png'),
+(53, 'upload/user1.png'),
+(54, 'upload/ps1.jpg'),
+(55, 'upload/user2.png'),
+(56, '');
+
 -- --------------------------------------------------------
 
 --
@@ -144,6 +182,7 @@ CREATE TABLE `images` (
 
 CREATE TABLE `patreg` (
   `pid` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
   `fname` varchar(20) NOT NULL,
   `lname` varchar(20) NOT NULL,
   `gender` varchar(10) NOT NULL,
@@ -152,16 +191,21 @@ CREATE TABLE `patreg` (
   `address` varchar(255) NOT NULL,
   `city` varchar(40) NOT NULL,
   `contact` varchar(10) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `cpassword` varchar(30) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `cpassword` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `patreg`
 --
 
-INSERT INTO `patreg` (`pid`, `fname`, `lname`, `gender`, `email`, `age`, `address`, `city`, `contact`, `password`, `cpassword`) VALUES
-(12, 'Akash', 'Sawarkar', 'Male', 'akash12345@gmail.com', '23', 'Street 101', 'Pune', '9986896896', 'Akash123', 'Akash123');
+INSERT INTO `patreg` (`pid`, `image`, `fname`, `lname`, `gender`, `email`, `age`, `address`, `city`, `contact`, `password`, `cpassword`) VALUES
+(13, '', 'Akash', 'Sawarkar', 'Male', 'akashsawarkar143@gmail.com', '23', 'Shivajinagar', 'Pune', '9898998998', 'ad8c932963c7af0ecacb204eeab1dc94', 'ad8c932963c7af0ecacb204eeab1dc94'),
+(14, '306473.png', 'Vivek', 'Salotkar', 'Male', 'viveksalotkar@gmail.com', '23', 'Baner', 'Pune', '9865321456', '7bee2a15709aadc8dae5a1141dc7f355', '7bee2a15709aadc8dae5a1141dc7f355'),
+(15, '', 'Sudeep', 'Baravkar', 'Male', 'sudeep@gmail.com', '22', 'Shivajinagar', 'Pune', '9858696868', '593323aa2ce57f697171896f7de653cd', '593323aa2ce57f697171896f7de653cd'),
+(16, '3.jpg', 'Ajinkya', 'Girigosavi', 'Male', 'ajinkya@gmail.com', '22', 'Shivajinagar', 'Pune', '8689698896', '0aa3bf4ffa71eddcb67dc7e51fe3e559', '0aa3bf4ffa71eddcb67dc7e51fe3e559'),
+(17, '2.jpg', 'Kiran', 'Kutwal', 'Male', 'kiran@gmail.com', '22', 'Shivajinagar', 'Pune', '6986896968', '6e30b7f957112daaa6f4f7c7dadd7be6', '6e30b7f957112daaa6f4f7c7dadd7be6'),
+(19, '', 'Manasi', 'Chatur', 'Female', 'manasi@gmail.com', '20', 'sangvi', 'Pune', '9889669999', 'dd16c0731752dc10d0c3e5dca8063fc5', 'dd16c0731752dc10d0c3e5dca8063fc5');
 
 -- --------------------------------------------------------
 
@@ -191,7 +235,62 @@ INSERT INTO `prestb` (`doctor`, `pid`, `ID`, `fname`, `lname`, `appdate`, `appti
 ('Ganesh', 2, 8, 'Alia', 'Bhatt', '2020-03-21', '10:00:00', 'Severe Fever', 'Nothing', 'Take bed rest'),
 ('Kumar', 9, 12, 'William', 'Blake', '2020-03-26', '12:00:00', 'Sever fever', 'nothing', 'Paracetamol -> 1 every morning and night'),
 ('Tiwary', 9, 13, 'William', 'Blake', '2020-03-26', '14:00:00', 'Cough', 'Skin dryness', 'Intake fruits with more water content'),
-('Sneha', 12, 14, 'Akash', 'Sawarkar', '2021-05-28', '14:00:00', 'Fever', 'n/a', 'Take medicine on time and rest');
+('Sneha', 12, 14, 'Akash', 'Sawarkar', '2021-05-28', '14:00:00', 'Fever', 'n/a', 'Take medicine on time and rest'),
+('Sneha', 14, 17, 'Vivek', 'Salotkar', '2021-06-18', '08:00:00', 'Dengue', 'None', 'Take medicines');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resetpasswords`
+--
+
+CREATE TABLE `resetpasswords` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resetpasswords`
+--
+
+INSERT INTO `resetpasswords` (`id`, `code`, `email`) VALUES
+(11, '160b3a00f59fc3', 'akashsawarkar143@gmail.com'),
+(0, '160b3a8087a78b', 'akashsawarkar143@gmail.com'),
+(0, '160b3aa067fe0f', 'akashsawarkar143@gmail.com'),
+(0, '160b3aa0e72633', 'akashsawarkar143@gmail.com'),
+(0, '160b3aa4d3c1f3', 'akashsawarkar143@gmail.com'),
+(0, '160be1b1bd45f6', ''),
+(0, '160be1b2263a02', ''),
+(0, '160be1c07001f1', ''),
+(0, '160be1c0bc6684', ''),
+(0, '160be1c14c5b8d', ''),
+(0, '160be1c716c369', 'akashsawarkar143@gmail.com'),
+(0, '160be1cea4bee5', ''),
+(0, '160be1cf112784', ''),
+(0, '160be1d402e802', ''),
+(0, '160be1d428b93f', ''),
+(0, '160be1d526ce46', ''),
+(0, '160be1d561cedd', ''),
+(0, '160be1d5b2d629', ''),
+(0, '160be1d63602c9', ''),
+(0, '160be1d74c9f51', ''),
+(0, '160be1d7ed39ca', ''),
+(0, '160be1d97a4aa8', 'akashsawarkar143@gmail.com'),
+(0, '160be1db67e2bd', ''),
+(0, '160be1dba188af', 'akashsawarkar143@gmail.com'),
+(0, '160be1eaa2d6f4', ''),
+(0, '160be1eada4aa6', 'aaa'),
+(0, '160be207a39255', 'akashsawarkar@gmail.com'),
+(0, '160be20957f44e', ''),
+(0, '160be2098c9869', ''),
+(0, '160be209aba0cf', ''),
+(0, '160be20ae57972', ''),
+(0, '160be20b1511a5', ''),
+(0, '160be20b44123e', ''),
+(0, '160be34a5924a4', 'akashsawarkar143@gmail.com'),
+(0, '160be35a80a6d9', ''),
+(0, '160be35adc9744', 'akashsawarkar143@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -223,19 +322,19 @@ ALTER TABLE `patreg`
 -- AUTO_INCREMENT for table `appointmenttb`
 --
 ALTER TABLE `appointmenttb`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `patreg`
 --
 ALTER TABLE `patreg`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
